acc77ccf0225e8ae2e4f54b0c6c20507212e7507
dujthep-odds <dujthep@odds.team>
2022-11-09 18:11:18 +0700

    change to GraphQLSchema

--- server.js
+++ server.js
@@ -1,75 +1,51 @@
 var express = require('express');
 var cors = require('cors')
 var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+const graphql = require('graphql');
 var app = express();
 
 app.use(cors())
 
-var schema = buildSchema(`
-  input InformationInput {
-    fullName: String
-    nickName: String
-  }
-
-  type Information {
-    id: Int!
-    fullName: String!
-    nickName: String!
-  }
-
-  type Query {
-    getInformation(id: ID!): Information
-    getAll: [Information]
-  }
-
-  type Mutation {
-    createInformation(input: InformationInput): Information
-    updateInformation(id: ID!, input: InformationInput): Information
-  }
-`);
-
-class Information {
-  constructor(id, { fullName, nickName }) {
-    this.id = id;
-    this.fullName = fullName;
-    this.nickName = nickName;
-  }
-}
-
 var fakeDatabase = [
-  { id: 0, fullName: 'Dujthep Taichan', nickName: 'Stop' }
+  { id: 0, fullName: 'Dujthep Taichan', nickName: 'Stop' },
+  { id: 1, fullName: 'Test Test', nickName: 'Rain' }
 ];
 
-var root = {
-  getInformation: ({ id }) => {
-    const data = fakeDatabase.find(r => r.id == id);
-    if (!data) {
-      throw new Error('no Information exists with id ' + id);
-    }
-    return new Information(id, fakeDatabase[id]);
-  },
-  createInformation: ({ input }) => {
-    const id = fakeDatabase.length;
-    fakeDatabase.push({...input, id: id});
-    return new Information(id, input);
-  },
-  updateInformation: ({ id, input }) => {
-    const data = fakeDatabase.find(r => r.id === id);
-    if (!data) {
-      throw new Error('no Information exists with id ' + id);
-    }
-    fakeDatabase[id] = {...input, id: fakeDatabase[id].id};
-    return new Information(id, input);
-  },
-  getAll: () => {
-    return fakeDatabase;
+var informationType = new graphql.GraphQLObjectType({
+  name: 'Information',
+  fields: {
+    id: { type: graphql.GraphQLInt },
+    fullName: { type: graphql.GraphQLString },
+    nickName: { type: graphql.GraphQLString },
   }
-};
+});
 
+var queryType = new graphql.GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    getInformation: {
+      type: informationType,
+      args: {
+        id: { type: graphql.GraphQLInt }
+      },
+      resolve: (_, {id}) => {
+        return fakeDatabase.find(r => r.id == id);
+      }
+    }, 
+    getAll: {
+      type: new graphql.GraphQLList(informationType),
+      resolve: () => {
+        return fakeDatabase;
+      }
+    }
+  }
+});
+
+var schema = new graphql.GraphQLSchema({query: queryType});
+
+var app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  rootValue: root,
   graphiql: true,
 }));
 app.listen(4000);
